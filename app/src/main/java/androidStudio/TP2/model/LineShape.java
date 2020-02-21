24fce87ec158b456cef3f87c2b5dc242b97b48bc
dmu01 <davymu@hotmail.fr>
2020-02-21 10:01:31 +0100

    Commit

---
+++ app/src/main/java/androidStudio/TP2/model/LineShape.java
@@ -0,0 +1,12 @@
+package androidStudio.TP2.model;
+
+import android.graphics.Canvas;
+
+public class LineShape implements DrawableShape {
+
+
+    @Override
+    public void drawShape(float left, float top, float right, float bottom, Canvas canvas) {
+
+    }
+}
---
+++ app/src/main/java/androidStudio/TP2/model/Place.java
@@ -0,0 +1,16 @@
+package androidStudio.TP2.model;
+
+public class Place {
+    private final float x1;
+    private final float x2;
+    private final float y1;
+    private final float y2;
+
+    public Place(float x1,float x2,float y1, float y2){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+}
