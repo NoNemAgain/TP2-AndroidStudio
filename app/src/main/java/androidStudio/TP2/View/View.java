3061c5566bb2a6923d530f793fbe784ae1812dba
dl-penguin <dillon.cheng@outlook.com>
2020-02-26 14:51:21 +0100

    ...

--- app/src/main/java/androidStudio/TP2/View/View.java
+++ app/src/main/java/androidStudio/TP2/View/View.java
@@ -2,18 +2,22 @@ package androidStudio.TP2.View;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import androidStudio.TP2.model.ShapeContainer;
+import androidStudio.TP2.tp2_androidstudio.ShapeContainerChangeListener;
 
 public class View extends android.view.View {
 
     private ShapeContainer model;
     private OnTouchListener otl;
 
+    private ShapeContainerChangeListener listener = () -> {this.invalidate();};
 
     public View(Context context) {
         super(context);
@@ -33,7 +37,12 @@ public class View extends android.view.View {
 
     public void setModel(ShapeContainer shapeContainer)
     {
+        if (this.model != null) {
+            this.model.removeChangeListener(listener);
+        }
+
         this.model = shapeContainer;
+        this.model.addChangeListener(listener);
         this.invalidate();
     }
     public void onDraw(Canvas canvas) {
