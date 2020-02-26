a869421f348615c8cd05221ae364af6fa75db929
NoNemAgain <thomasduong290699@gmail.com>
2020-02-26 14:12:40 +0100

    TouchListener fait par le prof View

--- app/src/main/java/androidStudio/TP2/tp2_androidstudio/MainActivity.java
+++ app/src/main/java/androidStudio/TP2/tp2_androidstudio/MainActivity.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 
 import androidStudio.TP2.View.View;
+import androidStudio.TP2.model.LineShape;
+import androidStudio.TP2.model.Place;
 import androidStudio.TP2.model.ShapeContainer;
 
 public class MainActivity extends AppCompatActivity {
-
+    private float startX ;
+    private float startY;
+    private float endX;
+    private float endY;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ShapeContainer container = new ShapeContainer();
+        final ShapeContainer container = new ShapeContainer();
 
         View v = findViewById(R.id.ViewXML);
         v.setModel(container);
@@ -23,12 +28,19 @@ public class MainActivity extends AppCompatActivity {
                 switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
                         // enregistrer event.x event.y dans startX et startY
+                         startX = event.getX();
+                        startY = event.getY();
                         return true;
                     case MotionEvent.ACTION_UP:
                         // enregistrer event.x eventY dans endX et endY
+                        endX = event.getX();
+                        endY = event.getY();
                         // créer new Place(startX, startY, endX, endY);
+                        Place p1 = new Place(startX,startY,endX,endY);
                         // créer new LineShape();
+                        LineShape ls1 = new LineShape();
                         // container.add(lineShape, place);
+                        container.add(ls1,p1);
                         return true;
                     case MotionEvent.ACTION_MOVE:
                         // pour le cursif
