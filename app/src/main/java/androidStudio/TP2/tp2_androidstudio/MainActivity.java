2092748cc1998a61c668f9bc990ec114c1f2143f
dl-penguin <dillon.cheng@outlook.com>
2020-02-26 14:16:13 +0100

    ...

--- app/src/main/java/androidStudio/TP2/tp2_androidstudio/MainActivity.java
+++ app/src/main/java/androidStudio/TP2/tp2_androidstudio/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final ShapeContainer container = new ShapeContainer();
 
         View v = findViewById(R.id.ViewXML);
